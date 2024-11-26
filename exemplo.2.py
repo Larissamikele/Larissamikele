677cf799171d7f639216f1eb501dd1186a1a8b50
Larissa Mikele <Lary.mikele@gmail.com>
2024-11-26 20:33:00 -0300

    Add files via upload

---
+++ exemplo.2.py
@@ -0,0 +1,12 @@
+''' exemplo 02: calculando e exibindo a soma dos 100 primeiros números inteiros positivos
+
+'''
+
+soma = 0 
+x    = 1
+
+while x <= 100: 
+    soma += x
+    x  += 1
+
+    print(f'a soma dos 100 primeiros números inteiros positivos é {soma}. ')
