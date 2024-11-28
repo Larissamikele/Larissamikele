19f242aaa923f0034a0adf6bf8132f43c41e3d6d
Larissa Mikele <Lary.mikele@gmail.com>
2024-11-28 16:14:15 -0300

    exemplo corrigido

--- exemplo.2.py
+++ exemplo.2.py
@@ -1,12 +1,11 @@
-''' exemplo 02: calculando e exibindo a soma dos 100 primeiros números inteiros positivos
+# Inicializando a variável soma e o contador x
+soma = 0
+x = 1
 
-'''
+# Laço de repetição que percorre os 100 primeiros números inteiros positivos
+while x <= 100:
+    soma += x  # Adiciona o valor de x à soma
+    x += 1     # Incrementa x em 1
 
-soma = 0 
-x    = 1
-
-while x <= 100: 
-    soma += x
-    x  += 1
-
-    print(f'a soma dos 100 primeiros números inteiros positivos é {soma}. ')
+# Exibindo o resultado após o fim do laço
+print(f'A soma dos 100 primeiros números inteiros positivos é {soma}.')
